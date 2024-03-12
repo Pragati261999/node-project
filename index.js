c4db0ad8cae21720644ac9c69e3954ddbb21c12e
CurePe <pragati.synchsofthq@gmail.com>
2024-03-12 18:21:50 +0530

    cors changes

--- index.js
+++ index.js
@@ -8,11 +8,21 @@ const userRoutes = require("../node-start/routes/user.route");
 
 const swaggerUi = require("swagger-ui-express");
 const swaggerSpecs = require("./config/swagger");
+const cors = require("cors");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpecs));
+// Enable CORS for all routes
+const corsOptions = {
+  origin: ["http://localhost:5173", "http://your-other-allowed-origin"],
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true, // Enable credentials (cookies, authorization headers)
+  optionsSuccessStatus: 204, // Some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+app.use(cors(corsOptions));
 
 // Add sequelize.authenticate() to ensure the database connection is established
 db.authenticate()
