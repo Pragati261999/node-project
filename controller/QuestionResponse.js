f216a2baed97ff54275413df23b000a42d67829a
CurePe <pragati.synchsofthq@gmail.com>
2024-03-30 23:15:15 +0530

    add question response api to save data

---
+++ controller/QuestionResponse.js
@@ -0,0 +1,19 @@
+const Subject = require("../models/Questions.model");
+
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const Questions = require("../models/QuestionResponses.model");
+
+const addquestionsresponses = async (req, res) => {
+  try {
+    const newQuestionResponse = await QuestionResponse.create(req.body);
+    res.status(201).json(newQuestionResponse);
+  } catch (error) {
+    console.error('Error creating question response:', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+};
+
+module.exports = {
+  addquestionsresponses,
+};
