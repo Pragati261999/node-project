d6d284e16beed8aafb83b37169d972705c01d6b7
CurePe <pragati.synchsofthq@gmail.com>
2024-03-31 22:16:06 +0530

    error handling cases

--- controller/subject.js
+++ controller/subject.js
@@ -5,19 +5,21 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
 const addsubject = async (req, res, next) => {
-  const { course_id, subject_name, subject_code } = req.body;
-  const addsubject = await Subject.create({
-    course_id,
-    subject_name,
-    subject_code,
-  });
-  res.status(200).json(addsubject);
+  try {
+    const { course_id, subject_name, subject_code } = req.body;
+    const addsubject = await Subject.create({
+      course_id,
+      subject_name,
+      subject_code,
+    });
+    res.status(200).json(addsubject);
+  } catch (error) {
+    // Handle errors
+    console.error("Error fetching subjects with course details:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
 };
 
-// const getsubject = async (req, res, next) => {
-//   const getsubject = await Subject.findAll();
-//   res.status(200).json(getsubject);
-// }
 const getsubject = async (req, res, next) => {
   try {
     // Fetch subjects from Subject table
@@ -29,8 +31,6 @@ const getsubject = async (req, res, next) => {
         const courseId = subject.course_id; // Assuming 'course_id' is the attribute in the Subject table
         const course = await Course.findOne({ where: { course_id: courseId } }); // Assuming 'id' is the primary key in the Course table
 
-
-        
         return {
           ...subject.toJSON(), // Convert subject object to JSON
           course: course ? course.toJSON() : null, // Include course details if found, else null
@@ -45,7 +45,6 @@ const getsubject = async (req, res, next) => {
     res.status(500).json({ error: "Internal server error" });
   }
 };
-
 
 module.exports = {
   addsubject,
